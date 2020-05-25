cf968a04f048d5640e7047afd0e741083d0aba09
Kavin Tjhan <ktjhan@Kavins-MBP.hsd1.ca.comcast.net>
2020-05-25 12:34:37 -0700

    fixed the rooms

--- public/js/main.js
+++ public/js/main.js
@@ -3,19 +3,21 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
+
+
 // Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
+const { username, room, newroom } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
 const socket = io();
 
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit('joinRoom', { username, room});
 
 // Get room and users
 socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
+  outputRoomName(room || newroom);
   outputUsers(users);
 });
 
