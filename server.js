7832604a690a654f69ff2fbcad799e3613e1130e
Kavin Tjhan <ktjhan@Kavins-MBP.hsd1.ca.comcast.net>
2020-05-25 12:36:26 -0700

    fixed the chatbot name

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'KChat Bot';
 
 // Run when client connects
 io.on('connection', socket => {
